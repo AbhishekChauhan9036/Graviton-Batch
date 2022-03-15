100f0bb71a5deab354f59aa59104dd1a014339ea
Abhi <abhishekchauhan9036@gmail.com>
2022-03-15 07:08:15 +0530

    commit message

---
+++ Advance-DSA-Assignment/Trees, Binary Tree - In Class/Sum-of-leaves.java
@@ -0,0 +1,30 @@
+/**
+ *    author:  Abhishek Chauhan
+ *    created: 14.03.2022 22:53:11      
+**
+
+/*
+ class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+ public static int sumOfLeaf(Node root)
+   {
+       // your code here
+       if(root == null) return 0;
+       int sum = 0;
+       if(root.left == null && root.right == null)
+           sum += root.data;
+       sum += sumOfLeaf(root.left);
+       sum += sumOfLeaf(root.right);
+       
+       return sum;
+   }
